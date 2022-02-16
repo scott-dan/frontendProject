cb733838f87d1476cda8346c6afe624e93608197
5NRLX <danieldscott90@gmail.com>
2022-02-15 18:29:45 -0800

    Add sample components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Ipsum from "./components/Ipsum";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
         <Header />
+        <Ipsum />
     </div>
   );
 }
