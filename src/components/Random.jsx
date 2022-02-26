79d595b59225abaadb0ddeb4bca7663acd99d048
Forest Pearson <flpearson.fp@gmail.com>
2022-02-26 05:37:09 -0800

    Format and API fetch skeleton

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-let url = "https://api.magicthegathering.io/v1/cards";
+//import Card from "mtgsdk";
+let url = "https://thronesapi.com/api/v2/Characters";
+//const mtg = require('mtgsdk')
 
 function Random() {
   return (
@@ -7,15 +9,13 @@ function Random() {
       <div className="container">
         <div className="row align-items-center my-5">
           <div className="col-lg-7">
-            <form>
-              <div className="col-lg-5">
-                <h1 className="font-weight-light">Generate</h1>
-                <p>
-                  This returns a random generated card with the ability to
-                  select from a category of predefined options to filter what
-                  type of random Card will be displayed
-                </p>
-              </div>
+            <div className="col-lg-5">
+              <h1 className="font-weight-light">Generate</h1>
+              <p>
+                This returns a random generated card with the ability to select
+                from a category of predefined options to filter what type of
+                random Card will be displayed
+              </p>
               <div className="card-type">
                 <label htmlFor="card-type">Card Type:</label>
                 <br />
@@ -28,7 +28,7 @@ function Random() {
                 <br />
               </div>
               <button onClick={rnd}>Generate</button>
-            </form>
+            </div>
           </div>
           <div className="col-lg-5">
             <h1 className="font-weight-light">Card Search</h1>
@@ -41,24 +41,60 @@ function Random() {
         </div>
       </div>
       <div className="randomizer">
-        <section className="randomResult"></section>
+        <div>
+          <p className="test" id="help">
+            {" "}
+            do I show
+          </p>
+        </div>
+
+        <section className="randomResult" id="randomResult">
+          <div>Hi</div>
+        </section>
       </div>
     </div>
   );
 }
-
 function rnd() {
-  let page = document.getElementsByClassName("randomResult")[0];
+  let page = document.getElementById("randomResult")[0];
+  //let tes = document.getElementsByClassName('test');
+  //let t = document.getElementById('help');
+  //console.log(t.innerHTML);
+  //console.log(tes.innerHTML);
+  //tes.innerHTML = "ddddd";
+  console.log("hello");
+  //while (page.firstChild) {
+  //  page.removeChild(page.firstChild);
+  //}
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  //.then(data => console.log(data.cards[1]));
 
-  while (page.firstChild) {
-    page.removeChild(page.firstChild);
-  }
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((element) => {
+        if (element.multiverseid === "130550") {
+          let result = document.crateElement("div");
+          let name = document.createElement("p");
+          name.classList.add("name");
+          name.innerHTML = element.name;
+          let image = document.createElement("img");
+          image.src = element.imageURL;
+          image.classList.add("image");
 
-  fetch(url).then((response) => response.json())
-  .then((data) => {
-    data.forEach((element) =>{
+          let test = document.createElement("p");
+          test.classList.add("name");
+          test.InnerHtml = "testing";
 
+          result.appendChild(test);
+          result.appendChild(name);
+          result.appendChild(image);
+
+          page.appendChild(result);
+        }
+      });
     });
-  });
 }
 export default Random;
