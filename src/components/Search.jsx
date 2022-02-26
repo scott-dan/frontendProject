fdc243c5d319c4fd10071d48fb20c8e81e9eefc3
Allen Golan <agolan377@gmail.com>
2022-02-26 07:20:58 -0800

    selecting search category makes search bar appear

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,116 @@
 import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Search() {
+let url = "https://api.magicthegathering.io/v1/";
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchCategory: "",
+      showSearchBar: false,
+      character: [],
+      inputValue: [],
+      foundCharacter: false,
+      count: 0,
+    };
+    this.handleAddingDivs = this.handleAddingDivs.bind(this);
+  }
+
+  updateInputValue(evt) {
+    const val = evt.target.value;
+    this.setState({
+      inputValue: val,
+    });
+  }
+
+  getData() {
+    console.log("url: ", url + this.state.searchCategory + "?name=" + this.state.inputValue);
+    console.log("url encoded input value: ", encodeURI(this.state.inputValue));
+    axios.get(url + encodeURI(this.state.inputValue)).then((response) => {
+      this.setState({
+        characters: response.data,
+      });
+      console.log(this.state.characters);
+      for (let i = 0; i < response.data.length; i++) {
+        if (response.data[i].fullName === this.state.inputValue) {
+          this.setState({ character: response.data[i] });
+          this.setState({ foundCharacter: true });
+          this.setState({ foundCharacter: true });
+          this.handleAddingDivs();
+          break;
+        }
+      }
+    });
+  }
+
+  handleAddingDivs() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  renderDivs() {
+    let uiItems = [];
+    if (this.state.foundCharacter) {
+      uiItems.push(
+        <div class="p-3">
+          <img
+            src={`${this.state.character.imageUrl}`}
+            alt="temp"
+            width="250"
+            height="250"
+          ></img>
+          <p class="text-white font-weight-bold">
+            {this.state.character.fullName}
+          </p>
+          <p class="text-white font-weight-bold">
+            {this.state.character.title}
+          </p>
+        </div>
+      );
+    }
+    return uiItems;
+  }
+
+  handleDDLChange(event) {
+    this.setState({ searchCategory: event.target.value, showSearchBar: true });
+  }
+
+  searchBar() {
     return (
-      <div className="about">
-        <div class="container">
-          <div class="row align-items-center my-5">
-            <div class="col-lg-7">
-              <form>
-                <div class="name">
-                <label for="name">Card Name</label><br />
-                <input type="text" id="name"/><br />
-                </div>
-                
-                <div class="card-type">
-                <label for="card-type">Card Type:</label><br/>
-                <select name="card-type" id="card-type">
-                    <option>Choose an Option</option>
-                    <option value="type1">Type 1</option>
-                    <option value="type2">Type 2</option>
-                    <option value="type3">Type 3</option>
-                    <option value="type4">Type 4</option>
-                </select><br/>
-                </div>
-                
-                <input type="submit" value="Submit" />
-                <input type="reset" value="Reset"/>
-                </form>
-            </div>
-            <div class="col-lg-5">
-              <h1 class="font-weight-light">Card Search</h1>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div>
+      <div>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={(evt) => this.updateInputValue(evt)}
+        ></input>
+        <button onClick={() => this.getData()}>Search</button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Searchpage</h1>
+        <div>
+          <select onChange={(event) => this.handleDDLChange(event)}>
+            <option value="none" selected disabled hidden>
+              Select a search category
+            </option>
+            <option>cards</option>
+            <option>sets</option>
+            <option>types</option>
+            <option>subtypes</option>
+            <option>supertypes</option>
+            <option>formats</option>
+          </select>
+          <br />
+          {this.state.showSearchBar ? this.searchBar() : null}
         </div>
       </div>
     );
   }
+}
 
 export default Search;
