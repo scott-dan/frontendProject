63a0e4ce80ff3e28a3e866839178ef750509cd0f
5NRLX <danieldscott90@gmail.com>
2022-02-27 23:58:51 -0800

    add section about illustrator

--- src/components/CardsByDan.jsx
+++ src/components/CardsByDan.jsx
@@ -1,4 +1,3 @@
-//import { card } from "mtgsdk";
 import React from "react";
 
 const url = "https://api.magicthegathering.io/v1/cards/?artist=dan+scott";
@@ -9,15 +8,23 @@ function CardsByDan() {
       <div className="container">
         <div className="row align-items-center my-5">
           <div className="col-lg-5">
-            <h1 className="font-weight-light">Cards By Dan</h1>
+            <h1 className="font-weight-bold">Cards By Dan</h1>
             <p>
               This returns a randomly selected card illustrated by the artist Dan Scott. Click the refresh button to load another card. {/*I think I'd like to change this to refresh on a set interval. Perhaps add settings for refresh timing and number of cards to display*/}
             </p>
-            <button onClick={GetCard}>Get New Card</button>
+            <button onClick={GetCard}>Get Card</button>
           </div>
-          <div className="col-lg-7">
-            <div className="cardImage text-center" id="cardImage"></div>
+          <div className="col-lg-7 text-center">
+            <img src="https://media.magic.wizards.com/image_legacy_migration/sideboard/images/usnat07/scott.jpg"
+            alt="Dan Scott MtG Artist"/>
+            <p>
+               Dan Scott is a freelance illustrator based out of the Kansas City, Missouri area. 
+               Dan is one of the more prolific Magic artists, having illustrated over 200 cards for Magic:The Gathering, 
+               including such iconic cards as Ponder, Solemn Simulacrum and Akroma's Memorial. 
+               He's painted dozens of Hearthstone cards as well.
+            </p>
       </div>
+      <div className="cardImage text-center" id="cardImage"></div>
       <div className="cardText text-center" id="cardText"></div>
         </div>
       </div>
@@ -69,8 +76,8 @@ function CardDisplay(data) {
     }
 }
 
-window.onload = (event) => {
+/* window.onload = function() {
     GetCard();
-}
+} */
 
 export default CardsByDan;
