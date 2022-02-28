486e310c653eafece86fd4a95bb2867fd0e0ef05
5NRLX <danieldscott90@gmail.com>
2022-02-27 22:19:13 -0800

    updated classnames and description text

--- src/components/CardsByDan.jsx
+++ src/components/CardsByDan.jsx
@@ -4,13 +4,13 @@ const url = "https://api.magicthegathering.io/v1/cards/?artist=dan+scott";
 
 function CardsByDan() {
   return (
-    <div className="about">
+    <div className="cardsByDan">
       <div className="container">
         <div className="row align-items-center my-5">
           <div className="col-lg-5">
             <h1 className="font-weight-light">Cards By Dan</h1>
             <p>
-              This returns a random generated card created by the artist Dan Scott. Click the refresh button to load another card.
+              This returns a randomly selected card illustrated by the artist Dan Scott. Click the refresh button to load another card. {/*I think I'd like to change this to refresh on a set interval. Perhaps add settings for refresh timing and number of cards to display*/}
             </p>
             <button onClick={NewCard}>Refresh</button>
           </div>
@@ -43,6 +43,6 @@ function NewCard() {
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 export default CardsByDan;
