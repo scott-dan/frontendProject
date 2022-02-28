62aa22e856d342b5d0c0edb570b40e8caa0ac630
5NRLX <danieldscott90@gmail.com>
2022-02-27 23:33:35 -0800

    pass single card to display func instead of array

--- src/components/CardsByDan.jsx
+++ src/components/CardsByDan.jsx
@@ -13,7 +13,7 @@ function CardsByDan() {
             <p>
               This returns a randomly selected card illustrated by the artist Dan Scott. Click the refresh button to load another card. {/*I think I'd like to change this to refresh on a set interval. Perhaps add settings for refresh timing and number of cards to display*/}
             </p>
-            <button onClick={GetCard}>Get Card</button>
+            <button onClick={GetCard}>Get New Card</button>
           </div>
           <div className="col-lg-7">
             <div className="cardImage text-center" id="cardImage"></div>
@@ -27,6 +27,7 @@ function CardsByDan() {
 
 function GetCard() {
     //console.log(url);
+    const rand = getRandomIntInclusive(0, 99);
     
     fetch(url)
       .then((response) => response.json())
@@ -35,7 +36,7 @@ function GetCard() {
           //console.log(rand);
           //console.log(data.cards[rand])
           //console.log(data.cards[rand].imageUrl)
-          CardDisplay(data);
+          CardDisplay(data.cards[rand]);
       });
     return;
 }
@@ -47,24 +48,24 @@ function getRandomIntInclusive(min, max) {
   }
 
 function CardDisplay(data) {
-    const rand = getRandomIntInclusive(0, 99);
+    const card = data;
     let imageContainer = document.getElementById("cardImage");
     let textContainer = document.getElementById("cardText");
-    console.log(data);
-    if(!data.cards[rand].hasOwnProperty('imageUrl')) {
+    console.log(card);
+    if(!card.hasOwnProperty('imageUrl')) {
         console.log("No image found");
-        imageContainer.innerHTML = `<h1>${data.cards[rand].name}</h1></br>
+        imageContainer.innerHTML = `<h1>${card.name}</h1></br>
           <p>No image found</p>`;
-        textContainer.innerHTML = `<p>${data.cards[rand].text}</P></br>
-          <p>${data.cards[rand].rarity}</P></br>
-          <p>${data.cards[rand].setName}</P></br>
-          <p>${data.cards[rand].type}</P></br>
-          <p>${data.cards[rand].subtypes}</P>`;
+        textContainer.innerHTML = `<p>${card.text}</P></br>
+          <p>${card.rarity}</P></br>
+          <p>${card.setName}</P></br>
+          <p>${card.type}</P></br>
+          <p>${card.subtypes}</P>`;
     } else {
-      imageContainer.innerHTML = `<img src =${data.cards[rand].imageUrl} /></br>`
-      textContainer.innerHTML = `<p>${data.cards[rand].name}</p></br>
-      <p>Rarity: ${data.cards[rand].rarity}</p></br>
-      <p>Card set: ${data.cards[rand].setName}</P></br>`;
+      imageContainer.innerHTML = `<img src =${card.imageUrl} /></br>`
+      textContainer.innerHTML = `<p>${card.name}</p></br>
+      <p>Rarity: ${card.rarity}</p></br>
+      <p>Card set: ${card.setName}</P></br>`;
     }
 }
 
