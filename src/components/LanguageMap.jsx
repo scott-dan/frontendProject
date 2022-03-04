ffbb6b6c26689857b51871bd246afcfd9cd4a87b
5NRLX <danieldscott90@gmail.com>
2022-03-03 16:27:42 -0800

    refine map display, state update for checkboxes

--- src/components/LanguageMap.jsx
+++ src/components/LanguageMap.jsx
@@ -3,6 +3,7 @@ import {
     ComposableMap,
     Geographies,
     Geography,
+    ZoomableGroup,
     Marker
   } from "react-simple-maps";
 
@@ -10,56 +11,61 @@ const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 const markers = [
-{ markerOffset: 15, name: "English", coordinates: [-77.0369, 38.9072] },
-{ markerOffset: 15, name: "Simplified Chinese", coordinates: [116.4074, 39.9042] },
-{ markerOffset: 15, name: "Traditional Chinese", coordinates: [114.1850, 36.0736] },
-{ markerOffset: 15, name: "French", coordinates: [2.3522, 48.8566] },
-{ markerOffset: 15, name: "German", coordinates: [13.4050, 52.5200] },
-{ markerOffset: 15, name: "Italian", coordinates: [9.1900, 45.4642] },
-{ markerOffset: 15, name: "Korean", coordinates: [126.9780, 37.5665] },
-{ markerOffset: 15, name: "Japanese", coordinates: [139.6503, 35.6762] },
-{ markerOffset: 15, name: "Portugese", coordinates: [9.1393, 38.7223] },
-{ markerOffset: 15, name: "Russian", coordinates: [37.6173, 55.7558] },
-{ markerOffset: 15, name: "Spanish", coordinates: [3.7038, 40.4168] },
+{ markerOffset: 7, name: "English", coordinates: [0.1276, 51.5072] },
+{ markerOffset: 7, name: "Simplified Chinese", coordinates: [116.4074, 39.9042] },
+{ markerOffset: 7, name: "Traditional Chinese", coordinates: [114.1850, 36.0736] },
+{ markerOffset: 7, name: "French", coordinates: [2.3522, 48.8566] },
+{ markerOffset: 7, name: "German", coordinates: [13.4050, 52.5200] },
+{ markerOffset: 7, name: "Italian", coordinates: [9.1900, 45.4642] },
+{ markerOffset: 7, name: "Korean", coordinates: [126.9780, 37.5665] },
+{ markerOffset: 7, name: "Japanese", coordinates: [139.6503, 35.6762] },
+{ markerOffset: 7, name: "Portugese", coordinates: [9.1393, 38.7223] },
+{ markerOffset: 7, name: "Russian", coordinates: [37.6173, 55.7558] },
+{ markerOffset: 7, name: "Spanish", coordinates: [3.7038, 40.4168] },
 ];
 
 const MapChart = () => {
     return (
       <ComposableMap>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => 
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill="#F5CE42"
-                  stroke="#42A1F5"
-                />
-              )
-          }
-        </Geographies>
-        {markers.map(({ name, coordinates, markerOffset }) => (
-          <Marker key={name} coordinates={coordinates}>
-            <g
-              fill="none"
-              stroke="#FF5533"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              transform="translate(-12, -24)"
-            >
-              <circle cx="12" cy="10" r="3" />
-              <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-            </g>
-            <text
-              textAnchor="middle"
-              y={markerOffset}
-              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-            >
-              {name}
-            </text>
-          </Marker>
-        ))}
+        <ZoomableGroup zoom={1.5} minZoom={1} maxZoom={5} center={[80, 30]}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies
+              .filter(d => d.properties.REGION_UN === "Europe" 
+                        || d.properties.REGION_UN === "Asia")
+              .map(geo => 
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#F5CE42"
+                    stroke="#42A1F5"
+                  />
+                )
+            }
+          </Geographies>
+          {markers.map(({ name, coordinates, markerOffset }) => (
+            <Marker key={name} coordinates={coordinates}>
+              <g
+                fill="none"
+                stroke="#FF5533"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transform="translate(-12, -24)"
+              >
+                <circle cx="12" cy="10" r="3" />
+                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+              </g>
+              <text
+                textAnchor="middle"
+                y={markerOffset}
+                style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+              >
+                {name}
+              </text>
+            </Marker>
+          ))}
+        </ZoomableGroup>
       </ComposableMap>
     );
   };
@@ -129,6 +135,17 @@ function Card(props){
         cardData: [],
         cardsToDisplay: false,
         displayCount: 3,
+        english: false,
+        chineseS: false,
+        chineseT: false,
+        french: false,
+        german: false,
+        italian: false,
+        korean: false,
+        japanese: false,
+        portugese: false,
+        russian: false,
+        spanish: false,
       };
     }
 
@@ -146,17 +163,17 @@ function Card(props){
           .catch((error) => this.setState({ apiCallError: true }));
       }
 
-    checkbox() {
-        return (
+    checkbox() { //add onchange event handlers for checkboxes to allow display toggle for language markers on map      
+      return (
             <form>
                 <div className="checklist">
-                    <input type="checkbox" id="english" />
+                    <input type="checkbox" id="english" onChange={(evt) => this.handleEvent(evt)}/>
                     <label>English</label><br />
                     <input type="checkbox" id="chineseS" />
                     <label>Chinese (Simplified)</label><br />
                     <input type="checkbox" id="chineseT" />
                     <label>Chinese (Traditional)</label><br />
-                    <input type="checkbox" id="french" />
+                    <input type="checkbox" id="french"/>
                     <label>French</label><br />
                     <input type="checkbox" id="german" />
                     <label>German</label><br />
@@ -179,6 +196,54 @@ function Card(props){
             </form>
         )
     }
+
+    handleEvent(evt) {
+      switch (evt.target.id) {
+        case 'english':
+          this.setState({english: evt.target.checked});
+          break;
+        case 'chineseS':
+          this.setState({chineseS: evt.target.checked});
+          break;
+        case 'chineseT':
+          this.setState({chineseT: evt.target.checked});
+          break;
+        case 'french':
+          this.setState({french: evt.target.checked});
+          break;
+          case 'german':
+        this.setState({german: evt.target.checked});
+        break;
+        case 'italian':
+          this.setState({italian: evt.target.checked});
+          break;
+        case 'korean':
+          this.setState({korean: evt.target.checked});
+          break;
+        case 'japanese':
+          this.setState({japanese: evt.target.checked});
+          break;
+        case 'portugese':
+          this.setState({portugese: evt.target.checked});
+          break;
+        case 'russian':
+          this.setState({russian: evt.target.checked});
+          break;
+        case 'spanish':
+          this.setState({spanish: evt.target.checked});
+          break;
+        default:
+          console.log('No checkbox interacted with');
+      }
+
+
+
+      /* console.log(checkboxState)
+      const val = evt.target.checked;
+      console.log(val)
+      this.setState({checkboxState: val,});
+      console.log(this.state.english) */
+    }  
 
     render() {
         return (
