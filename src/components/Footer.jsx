d914de2479970918cb9d50c5b13e6819a41a7190
5NRLX <danieldscott90@gmail.com>
2022-03-06 20:42:38 -0800

    fixed padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../components/styles/footerStyles.css";
 function Footer() {
   return (
     <div className="footer">
-      <footer className="py-1 fixed-bottom">
+      <footer className="py-3 fixed-bottom">
         <div className="container">
           <p className="m-0 text-center text-white">
             Copyright &copy; Bob Loblaw 2022
